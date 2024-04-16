675768bf80da006e0245fbffbbc7f9a8c375107d
0xf333 <0x333@tuta.io>
2024-04-16 04:25:09 -0400

    fix: copy `polkavm_guest.bc` to output directory

--- crates/pallet-contracts-pvm-llapi/build.rs
+++ crates/pallet-contracts-pvm-llapi/build.rs
@@ -35,8 +35,19 @@ fn main() {
     let bitcode_path = Path::new(&out_dir).join(lib);
     compile(bitcode_path.to_str().expect("$OUT_DIR should be UTF-8"));
 
-    let bitcode = fs::read(bitcode_path).expect("bitcode should have been built");
+    let bitcode = fs::read(&bitcode_path).expect("bitcode should have been built");
     let len = bitcode.len();
+
+    // Copying the polkavm_guest.bc file to the current directory
+    // for now, I will change this to copy to the output directory
+    // specified by the user.
+    // e.g:
+    // `cargo run Storage.sol -O3 --bin --output-dir './out'` 
+    // should copy the bitcode to `./out`. 
+    let current_dir = env::current_dir().expect("should be able to get current directory");
+    let output_path = current_dir.join(lib);
+    fs::copy(&bitcode_path, &output_path).expect("should be able to copy bitcode to current directory");
+
     let src_path = Path::new(&out_dir).join("polkavm_guest.rs");
     let src = format!("pub static BITCODE: &[u8; {len}] = include_bytes!(\"{lib}\");");
     fs::write(src_path, src).expect("should be able to write in $OUT_DIR");

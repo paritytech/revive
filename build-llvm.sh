8a6d43bcd3eb5af8a618ee0c7e9751aa1b1b5718
Cyrill Leutwiler <bigcyrill@hotmail.com>
2024-04-16 07:55:06 +0200

    LLVM build script: Use LLVM 18 branch since compiler-rt fix got backported
    Signed-off-by: Cyrill Leutwiler <bigcyrill@hotmail.com>

--- build-llvm.sh
+++ build-llvm.sh
@@ -6,9 +6,9 @@ INSTALL_DIR="${PWD}/llvm18.0"
 mkdir -p $INSTALL_DIR
 
 
-# Clone LLVM (any revision after commit bd32aaa is supposed to work)
+# Clone LLVM 18 (any revision after commit bd32aaa is supposed to work)
 if [ ! -d "llvm-project" ]; then
-  git clone --depth 1 --branch main https://github.com/llvm/llvm-project.git
+  git clone --depth 1 --branch release/18.x https://github.com/llvm/llvm-project.git
 fi
 
 

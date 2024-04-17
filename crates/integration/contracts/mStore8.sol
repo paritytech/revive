99d22cde2f4a81ac243edfd2b7f5fc70a07dcf62
xermicus <cyrill@parity.io>
2024-04-17 12:48:19 +0200

    provide Solidity source for mstore8 tests
    Signed-off-by: xermicus <cyrill@parity.io>

---
+++ crates/integration/contracts/mStore8.sol
@@ -0,0 +1,8 @@
+contract MStore8 {
+    function mStore8(uint value) public pure returns (uint256 word) {
+        assembly {
+            mstore8(0x80, value)
+            word := mload(0x80)
+        }
+    }
+}

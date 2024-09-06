c04c2560acc4047ffe7705d66638025a58764385
Cyrill Leutwiler <bigcyrill@hotmail.com>
2024-09-06 09:55:43 +0200

    wip
    Signed-off-by: Cyrill Leutwiler <bigcyrill@hotmail.com>

--- crates/integration/contracts/Call.sol
+++ crates/integration/contracts/Call.sol
@@ -2,6 +2,40 @@
 
 pragma solidity ^0.8;
 
+/* runner.json
+{
+    "differential": true,
+    "actions": [
+        {
+            "Instantiate": {
+                "code": {
+                    "Solidity": {
+                        "contract": "Call"
+                    }
+                }
+            }
+        },
+        {
+            "Instantiate": {
+                "code": {
+                    "Solidity": {
+                        "contract": "Call"
+                    }
+                }
+            }
+        },
+        {
+            "Call": {
+                "dest": {
+                    "Instantiated": 1
+                },
+                "data": "1b8b921d0000000000000000000000001c81a61a407017c58397a47d2ab28191b9b8ec9b000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000050102030405000000000000000000000000000000000000000000000000000000"
+            }
+        }
+    ]
+}
+*/
+
 contract Call {
     function value_transfer(address payable destination) public payable {
         destination.transfer(msg.value);

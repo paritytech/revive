d28db269f4144e5fe0e06c98eafe91c00acc4d06
xermicus <cyrill@parity.io>
2024-10-31 12:18:21 +0100

    include the llvm version in the long version string
    Signed-off-by: xermicus <cyrill@parity.io>

--- crates/solidity/src/version.rs
+++ crates/solidity/src/version.rs
@@ -10,16 +10,21 @@ pub struct Version {
     pub long: String,
     /// The short `semver`.
     pub default: semver::Version,
+    /// The LLVM version string.
+    pub llvm: semver::Version,
 }
 
 impl Default for Version {
     fn default() -> Self {
         let default = semver::Version::parse(env!("CARGO_PKG_VERSION")).expect("Always valid");
         let commit = env!("GIT_COMMIT_HASH");
+        let (llvm_major, llvm_minor, llvm_patch) = inkwell::support::get_llvm_version();
+        let llvm = semver::Version::new(llvm_major as u64, llvm_minor as u64, llvm_patch as u64);
 
         Self {
-            long: format!("{default}+commit.{commit}"),
+            long: format!("{default}+commit.{commit}..llvm-{llvm}"),
             default,
+            llvm,
         }
     }
 }

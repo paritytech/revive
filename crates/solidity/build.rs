4e5482bad272610d58410ea950234409c6797648
Cyrill Leutwiler <cyrill@parity.io>
2024-12-03 12:34:17 +0100

    fix the commit sha in the version (#129)
    Signed-off-by: xermicus <cyrill@parity.io>

--- crates/solidity/build.rs
+++ crates/solidity/build.rs
@@ -1,9 +1,7 @@
 fn main() {
-    let git_rev = std::process::Command::new("git")
-        .args(["rev-parse", "--short", "HEAD"])
-        .output()
-        .map(|out| String::from_utf8(out.stdout).unwrap_or_default())
-        .unwrap_or("unknown".to_owned());
-
-    println!("cargo:rustc-env=GIT_COMMIT_HASH={}", git_rev.trim());
+    let repo = git2::Repository::open("../..").expect("should be a repository");
+    let head = repo.head().expect("should have head");
+    let commit = head.peel_to_commit().expect("should have commit");
+    let id = &commit.id().to_string()[..7];
+    println!("cargo:rustc-env=GIT_COMMIT_HASH={id}");
 }

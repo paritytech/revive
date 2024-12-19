d8752ec6b59aa960ef4b4c542d695a68ef5ad99e
Cyrill Leutwiler <cyrill@parity.io>
2024-12-19 18:59:10 +0100

    the basefee opcode (#142)
    Signed-off-by: xermicus <cyrill@parity.io>

---
+++ crates/integration/contracts/BaseFee.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8;
+
+/* runner.json
+{
+    "differential": false,
+    "actions": [
+        {
+            "Instantiate": {
+                "code": {
+                    "Solidity": {
+                        "contract": "BaseFee"
+                    }
+                }
+            }
+        },
+        {
+            "VerifyCall": {
+                "success": true
+            }
+        }
+    ]
+}
+*/
+
+contract BaseFee {
+    constructor() payable {
+        assert(block.basefee == 0);
+    }
+}

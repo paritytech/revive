04b082d501cf19dfb01e1242e0b24028159506f0
Sebastian Miasojed <sebastian.miasojed@parity.io>
2025-01-08 21:00:54 +0100

    Add revive js tests

---
+++ js/examples/node/revive.js
@@ -0,0 +1,32 @@
+const soljson = require('solc/soljson');
+const createRevive = require('./resolc.js');
+
+async function compile(standardJsonInput) {
+  if (!standardJsonInput) {
+    throw new Error('Input JSON for the Solidity compiler is required.');
+  }
+
+  // Initialize the compiler
+  const compiler = createRevive();
+  compiler.soljson = soljson;
+
+  // Provide input to the compiler
+  compiler.writeToStdin(JSON.stringify(standardJsonInput));
+
+  // Run the compiler
+  compiler.callMain(['--standard-json']);
+
+  // Collect output
+  const stdout = compiler.readFromStdout();
+  const stderr = compiler.readFromStderr();
+
+  // Check for errors and throw if stderr exists
+  if (stderr) {
+    throw new Error(`Compilation failed: ${stderr}`);
+  }
+
+  // Return the output if no errors
+  return stdout;
+}
+
+module.exports = { compile };

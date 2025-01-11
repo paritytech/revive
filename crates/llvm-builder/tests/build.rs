567a4b7a40b4b3141a117e69067ed1a4daa22ef6
xermicus <bigcyrill@hotmail.com>
2025-01-11 21:36:05 +0100

    fix tests: less brittle and remove futile cases
    Signed-off-by: xermicus <bigcyrill@hotmail.com>

--- crates/llvm-builder/tests/build.rs
+++ crates/llvm-builder/tests/build.rs
@@ -3,115 +3,53 @@ pub mod common;
 use std::process::Command;
 
 use assert_cmd::prelude::*;
-use predicates::prelude::*;
-use rstest::rstest;
 
-/// Tests building without cloning LLVM repository.
-///
-/// This test verifies that the build process fails when attempting to build LLVM without
-/// cloning the repository first.
-///
-/// # Errors
-///
-/// Returns an error if any of the test assertions fail or if there is an error while executing
-/// the build command.
-///
-/// # Returns
-///
-/// Returns `Ok(())` if the test passes.
-#[rstest]
-fn build_without_clone() -> anyhow::Result<()> {
-    let mut cmd = Command::cargo_bin(common::REVIVE_LLVM)?;
-    let file = assert_fs::NamedTempFile::new(common::LLVM_LOCK_FILE)?;
-    let path = file.parent().expect("Lockfile parent dir does not exist");
-    cmd.current_dir(path);
-    cmd.arg("build");
-    cmd.assert()
-        .failure()
-        .stderr(predicate::str::contains("building cmake failed"))
-        .stderr(predicate::str::is_match("The source directory.*does not exist").unwrap());
-    Ok(())
-}
-
-/// Tests the clone, build, and clean process of the LLVM repository.
-///
 /// This test verifies that the LLVM repository can be successfully cloned, built, and cleaned.
-///
-/// # Errors
-///
-/// Returns an error if any of the test assertions fail or if there is an error while executing
-/// the build or clean commands.
-///
-/// # Returns
-///
-/// Returns `Ok(())` if the test passes.
-#[rstest]
-#[timeout(std::time::Duration::from_secs(5000))]
+#[test]
 fn clone_build_and_clean() -> anyhow::Result<()> {
-    let mut cmd = Command::cargo_bin(common::REVIVE_LLVM)?;
-    let lockfile = common::create_test_tmp_lockfile(None)?;
-    let test_dir = lockfile
-        .parent()
-        .expect("Lockfile parent dir does not exist");
-    cmd.current_dir(test_dir);
-    cmd.arg("clone");
-    cmd.assert()
-        .success()
-        .stderr(predicate::str::is_match(".*Updating files:.*100%.*done").unwrap());
+    let test_dir = common::TestDir::with_lockfile(None)?;
 
-    let mut build_cmd = Command::cargo_bin(common::REVIVE_LLVM)?;
-    build_cmd.current_dir(test_dir);
-    build_cmd
+    Command::cargo_bin(common::REVIVE_LLVM)?
+        .current_dir(test_dir.path())
+        .arg("clone")
+        .assert()
+        .success();
+
+    Command::cargo_bin(common::REVIVE_LLVM)?
+        .current_dir(test_dir.path())
         .arg("build")
         .arg("--llvm-projects")
         .arg("clang")
         .arg("--llvm-projects")
         .arg("lld")
         .assert()
-        .success()
-        .stdout(predicate::str::is_match("Installing:.*").unwrap());
+        .success();
 
-    let mut builtins_cmd = Command::cargo_bin(common::REVIVE_LLVM)?;
-    builtins_cmd.current_dir(test_dir);
-    builtins_cmd
+    Command::cargo_bin(common::REVIVE_LLVM)?
+        .current_dir(test_dir.path())
         .arg("builtins")
         .assert()
-        .success()
-        .stdout(predicate::str::is_match("Installing:.*builtins-riscv64.a").unwrap());
+        .success();
 
-    let mut clean_cmd = Command::cargo_bin(common::REVIVE_LLVM)?;
-    clean_cmd.current_dir(test_dir);
-    clean_cmd.arg("clean");
-    clean_cmd.assert().success();
+    Command::cargo_bin(common::REVIVE_LLVM)?
+        .current_dir(test_dir.path())
+        .arg("clean")
+        .assert()
+        .success();
 
     Ok(())
 }
 
-/// Tests the clone, build, and clean process of the LLVM repository for MUSL target.
-///
 /// This test verifies that the LLVM repository can be successfully cloned, built, and cleaned
 /// with 2-staged build using MUSL as sysroot.
-///
-/// # Errors
-///
-/// Returns an error if any of the test assertions fail or if there is an error while executing
-/// the build or clean commands.
-///
-/// # Returns
-///
-/// Returns `Ok(())` if the test passes.
-#[rstest]
-#[timeout(std::time::Duration::from_secs(7200))]
-#[cfg(any(target_os = "linux", target_os = "macos"))]
+#[test]
+#[cfg(target_os = "linux")]
 fn clone_build_and_clean_musl() -> anyhow::Result<()> {
-    let lockfile = common::create_test_tmp_lockfile(None)?;
-    let test_dir = lockfile
-        .parent()
-        .expect("Lockfile parent dir does not exist");
+    let test_dir = common::TestDir::with_lockfile(None)?;
 
     Command::cargo_bin(common::REVIVE_LLVM)?
         .arg("clone")
-        .current_dir(test_dir)
+        .current_dir(test_dir.path())
         .assert()
         .success();
 
@@ -123,18 +61,18 @@ fn clone_build_and_clean_musl() -> anyhow::Result<()> {
         .arg("clang")
         .arg("--llvm-projects")
         .arg("lld")
-        .current_dir(test_dir)
+        .current_dir(test_dir.path())
         .assert()
         .success();
 
     Command::cargo_bin(common::REVIVE_LLVM)?
-        .current_dir(test_dir)
+        .current_dir(test_dir.path())
         .arg("builtins")
         .assert()
         .success();
 
     Command::cargo_bin(common::REVIVE_LLVM)?
-        .current_dir(test_dir)
+        .current_dir(test_dir.path())
         .arg("clean")
         .assert()
         .success();
@@ -142,106 +80,58 @@ fn clone_build_and_clean_musl() -> anyhow::Result<()> {
     Ok(())
 }
 
-/// Tests the debug build process of the LLVM repository with tests and coverage enabled.
-///
 /// This test verifies that the LLVM repository can be successfully cloned and built in debug mode
 /// with tests and coverage enabled.
-///
-/// # Errors
-///
-/// Returns an error if any of the test assertions fail or if there is an error while executing
-/// the build commands.
-///
-/// # Returns
-///
-/// Returns `Ok(())` if the test passes.
-#[rstest]
-#[timeout(std::time::Duration::from_secs(10000))]
+#[test]
 fn debug_build_with_tests_coverage() -> anyhow::Result<()> {
-    let mut cmd = Command::cargo_bin(common::REVIVE_LLVM)?;
-    let lockfile = common::create_test_tmp_lockfile(None)?;
-    let test_dir = lockfile
-        .parent()
-        .expect("Lockfile parent dir does not exist");
-    cmd.current_dir(test_dir);
-    cmd.arg("clone");
-    cmd.assert()
-        .success()
-        .stderr(predicate::str::is_match(".*Updating files:.*100%.*done").unwrap());
+    let test_dir = common::TestDir::with_lockfile(None)?;
 
-    let mut build_cmd = Command::cargo_bin(common::REVIVE_LLVM)?;
-    build_cmd.current_dir(test_dir);
-    build_cmd
+    Command::cargo_bin(common::REVIVE_LLVM)?
+        .current_dir(test_dir.path())
+        .arg("clone")
+        .assert()
+        .success();
+
+    Command::cargo_bin(common::REVIVE_LLVM)?
+        .current_dir(test_dir.path())
         .arg("build")
         .arg("--enable-coverage")
         .arg("--enable-tests")
         .arg("--build-type")
-        .arg("Debug");
-
-    build_cmd
+        .arg("Debug")
         .assert()
-        .success()
-        .stdout(predicate::str::is_match("Installing:.*").unwrap());
+        .success();
 
     Ok(())
 }
 
-/// Tests LLVM build with address sanitizer enabled.
-///
 /// This test verifies that the LLVM repository can be successfully built with address sanitizer.
-///
-/// # Errors
-///
-/// Returns an error if any of the test assertions fail or if there is an error while executing
-/// the build commands.
-///
-/// # Returns
-///
-/// Returns `Ok(())` if the test passes.
-#[rstest]
-#[timeout(std::time::Duration::from_secs(10000))]
+#[test]
 fn build_with_sanitizers() -> anyhow::Result<()> {
-    let mut cmd = Command::cargo_bin(common::REVIVE_LLVM)?;
-    let lockfile = common::create_test_tmp_lockfile(None)?;
-    let test_dir = lockfile
-        .parent()
-        .expect("Lockfile parent dir does not exist");
-    cmd.current_dir(test_dir);
-    cmd.arg("clone");
-    cmd.assert()
-        .success()
-        .stderr(predicate::str::is_match(".*Updating files:.*100%.*done").unwrap());
+    let test_dir = common::TestDir::with_lockfile(None)?;
 
-    let mut build_cmd = Command::cargo_bin(common::REVIVE_LLVM)?;
-    build_cmd.current_dir(test_dir);
-    build_cmd.arg("build").arg("--sanitizer").arg("Address");
-    build_cmd
+    Command::cargo_bin(common::REVIVE_LLVM)?
+        .current_dir(test_dir.path())
+        .arg("clone")
         .assert()
-        .success()
-        .stdout(predicate::str::is_match("Installing:.*").unwrap());
+        .success();
+
+    Command::cargo_bin(common::REVIVE_LLVM)?
+        .current_dir(test_dir.path())
+        .arg("build")
+        .arg("--sanitizer")
+        .arg("Address")
+        .assert()
+        .success();
 
     Ok(())
 }
 
 /// Tests the clone, build, and clean process of the LLVM repository for the emscripten target.
-///
-/// # Errors
-///
-/// Returns an error if any of the test assertions fail or if there is an error while executing
-/// the build or clean commands.
-///
-/// # Returns
-///
-/// Returns `Ok(())` if the test passes.
+#[test]
 #[cfg(any(target_os = "linux", target_os = "macos"))]
-#[rstest]
-#[timeout(std::time::Duration::from_secs(7200))]
 fn clone_build_and_clean_emscripten() -> anyhow::Result<()> {
-    let lockfile = common::create_test_tmp_lockfile(None)?;
-    let test_dir = lockfile
-        .parent()
-        .expect("Lockfile parent dir does not exist");
-
+    let test_dir = common::TestDir::with_lockfile(None)?;
     let command = Command::cargo_bin(common::REVIVE_LLVM)?;
     let program = command.get_program().to_string_lossy();
     let emsdk_wrapped_build_command = format!(
@@ -254,13 +144,13 @@ fn clone_build_and_clean_emscripten() -> anyhow::Result<()> {
     Command::new("sh")
         .arg("-c")
         .arg(emsdk_wrapped_build_command)
-        .current_dir(test_dir)
+        .current_dir(test_dir.path())
         .assert()
         .success();
 
     Command::cargo_bin(common::REVIVE_LLVM)?
         .arg("clean")
-        .current_dir(test_dir)
+        .current_dir(test_dir.path())
         .assert()
         .success();
 
--- crates/llvm-builder/tests/clone.rs
+++ crates/llvm-builder/tests/clone.rs
@@ -3,121 +3,34 @@ pub mod common;
 use std::process::Command;
 
 use assert_cmd::prelude::*;
-use predicates::prelude::*;
-use rstest::rstest;
 
-/// Tests the cloning process of the LLVM repository using a specific branch and reference.
-///
 /// This test verifies that the LLVM repository can be successfully cloned using a specific branch
 /// and reference.
-///
-/// # Errors
-///
-/// Returns an error if any of the test assertions fail or if there is an error while executing
-/// the clone command.
-///
-/// # Returns
-///
-/// Returns `Ok(())` if the test passes.
-#[rstest]
+#[test]
 fn clone() -> anyhow::Result<()> {
-    let mut cmd = Command::cargo_bin(common::REVIVE_LLVM)?;
-    let lockfile = common::create_test_tmp_lockfile(None)?;
-    let test_dir = lockfile
-        .parent()
-        .expect("Lockfile parent dir does not exist");
-    cmd.current_dir(test_dir);
-    cmd.arg("clone");
-    cmd.assert()
-        .success()
-        .stderr(predicate::str::is_match(".*Updating files:.*100%.*done").unwrap());
+    let test_dir = common::TestDir::with_lockfile(None)?;
+
+    Command::cargo_bin(common::REVIVE_LLVM)?
+        .current_dir(test_dir.path())
+        .arg("clone")
+        .assert()
+        .success();
+
     Ok(())
 }
 
-/// Tests the full cloning process of the LLVM repository using a specific branch and reference.
-///
 /// This test verifies that the LLVM repository can be successfully cloned using a specific branch
 /// and reference with --deep option.
-///
-/// # Errors
-///
-/// Returns an error if any of the test assertions fail or if there is an error while executing
-/// the clone command.
-///
-/// # Returns
-///
-/// Returns `Ok(())` if the test passes.
-#[rstest]
+#[test]
 fn clone_deep() -> anyhow::Result<()> {
-    let mut cmd = Command::cargo_bin(common::REVIVE_LLVM)?;
-    let lockfile = common::create_test_tmp_lockfile(None)?;
-    let test_dir = lockfile
-        .parent()
-        .expect("Lockfile parent dir does not exist");
-    cmd.current_dir(test_dir);
-    cmd.arg("clone");
-    cmd.arg("--deep");
-    cmd.assert()
-        .success()
-        .stderr(predicate::str::is_match(".*Updating files:.*100%.*done").unwrap());
-    Ok(())
-}
+    let test_dir = common::TestDir::with_lockfile(None)?;
 
-/// Tests the cloning process of the LLVM repository using an invalid reference.
-///
-/// This test verifies that attempting to clone the LLVM repository using an invalid reference
-/// results in a failure.
-///
-/// # Errors
-///
-/// Returns an error if any of the test assertions fail or if there is an error while executing
-/// the clone command.
-///
-/// # Returns
-///
-/// Returns `Ok(())` if the test passes.
-#[rstest]
-fn clone_wrong_reference() -> anyhow::Result<()> {
-    let mut cmd = Command::cargo_bin(common::REVIVE_LLVM)?;
-    let lockfile = common::create_test_tmp_lockfile(Some(
-        common::REVIVE_LLVM_REPO_TEST_SHA_INVALID.to_string(),
-    ))?;
-    let test_dir = lockfile
-        .parent()
-        .expect("Lockfile parent dir does not exist");
-    cmd.current_dir(test_dir);
-    cmd.arg("clone");
-    cmd.assert().failure().stderr(predicate::str::contains(
-        "Error: LLVM repository commit checking out failed",
-    ));
-    Ok(())
-}
+    Command::cargo_bin(common::REVIVE_LLVM)?
+        .current_dir(test_dir.path())
+        .arg("clone")
+        .arg("--deep")
+        .assert()
+        .success();
 
-/// Tests the cloning process of the LLVM repository without a lock file.
-///
-/// This test verifies that attempting to clone the LLVM repository without a lock file
-/// results in a failure.
-///
-/// # Errors
-///
-/// Returns an error if any of the test assertions fail or if there is an error while executing
-/// the clone command.
-///
-/// # Returns
-///
-/// Returns `Ok(())` if the test passes.
-#[rstest]
-fn clone_without_lockfile() -> anyhow::Result<()> {
-    let mut cmd = Command::cargo_bin(common::REVIVE_LLVM)?;
-    let file = assert_fs::NamedTempFile::new(common::LLVM_LOCK_FILE)?;
-    let path = file.parent().expect("Lockfile parent dir does not exist");
-    cmd.current_dir(path);
-    cmd.arg("clone");
-    cmd.assert()
-        .failure()
-        .stderr(predicate::str::contains(format!(
-            "Error: Error opening \"{}\" file",
-            common::LLVM_LOCK_FILE
-        )));
     Ok(())
 }

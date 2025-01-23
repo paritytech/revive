66534f4e8cf69d78606206d979f38e07e90e7d97
Sebastian Miasojed <sebastian.miasojed@parity.io>
2025-01-23 14:46:04 +0100

    Allow GC to do the cleanup

--- js/embed/soljson_interface.js
+++ js/embed/soljson_interface.js
@@ -13,6 +13,9 @@ mergeInto(LibraryManager.library, {
   resolc_compile: function (inputPtr, inputLen) {
     const inputJson = UTF8ToString(inputPtr, inputLen);
     var revive = createRevive();
+    // Allow GC to clean up the data
+    revive.wasmBinary = undefined
+    revive.soljson = undefined
     revive.writeToStdin(inputJson);
 
     // Call main on the new instance

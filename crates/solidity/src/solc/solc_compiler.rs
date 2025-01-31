4e1543fff1afffbd718d5acbf6a97ad9afd148ba
xermicus <cyrill@parity.io>
2025-01-31 10:46:56 +0100

    remove superfluous version checks
    Signed-off-by: xermicus <cyrill@parity.io>

--- crates/solidity/src/solc/solc_compiler.rs
+++ crates/solidity/src/solc/solc_compiler.rs
@@ -8,9 +8,9 @@ use crate::solc::combined_json::CombinedJson;
 use crate::solc::standard_json::input::Input as StandardJsonInput;
 use crate::solc::standard_json::output::Output as StandardJsonOutput;
 use crate::solc::version::Version;
+use crate::solc::FIRST_INCLUDE_PATH_VERSION;
 
 use super::Compiler;
-use crate::solc::{FIRST_SUPPORTS_BASE_PATH, FIRST_SUPPORTS_INCLUDE_PATH};
 
 /// The Solidity compiler.
 pub struct SolcCompiler {
@@ -50,40 +50,31 @@ impl Compiler for SolcCompiler {
         include_paths: Vec<String>,
         allow_paths: Option<String>,
     ) -> anyhow::Result<StandardJsonOutput> {
-        let version = self.version()?;
+        let version = self.version()?.default;
+
+        if !include_paths.is_empty() && version < FIRST_INCLUDE_PATH_VERSION {
+            anyhow::bail!("--include-path is not supported in solc {version}");
+        }
 
         let mut command = std::process::Command::new(self.executable.as_str());
         command.stdin(std::process::Stdio::piped());
         command.stdout(std::process::Stdio::piped());
         command.arg("--standard-json");
 
-        if let Some(base_path) = &base_path {
-            if !FIRST_SUPPORTS_BASE_PATH.matches(&version.default) {
-                anyhow::bail!(
-                    "--base-path not supported this version {} of solc",
-                    &version.default
-                );
-            }
-            command.arg("--base-path").arg(base_path);
-        }
-
-        if !include_paths.is_empty() && !FIRST_SUPPORTS_INCLUDE_PATH.matches(&version.default) {
-            anyhow::bail!(
-                "--include-path not supported this version {} of solc",
-                &version.default
-            );
-        }
-
         for include_path in include_paths.into_iter() {
             command.arg("--include-path");
             command.arg(include_path);
+        }
+        if let Some(base_path) = base_path {
+            command.arg("--base-path");
+            command.arg(base_path);
         }
         if let Some(allow_paths) = allow_paths {
             command.arg("--allow-paths");
             command.arg(allow_paths);
         }
 
-        input.normalize(&version.default);
+        input.normalize(&version);
 
         let suppressed_warnings = input.suppressed_warnings.take().unwrap_or_default();
 

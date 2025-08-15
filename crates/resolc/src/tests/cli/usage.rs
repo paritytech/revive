43c4da3a7db1369405021b7d95135c62168fd717
elle-j <linnea@parity.io>
2025-08-15 22:30:22 +0200

    Add usage test.

--- crates/resolc/src/tests/cli/usage.rs
+++ crates/resolc/src/tests/cli/usage.rs
@@ -5,6 +5,27 @@
 use crate::tests::cli::utils;
 
 #[test]
+#[ignore = "Fix: 'resolc --help' should exit with success exit code"]
+fn shows_usage_with_help() {
+    const ARGUMENTS: &[&str] = &["--help"];
+    let resolc_result = utils::execute_resolc(ARGUMENTS);
+    assert!(
+        resolc_result.success,
+        "Providing the `--help` option should succeed with exit code {}, got {}.",
+        revive_common::EXIT_CODE_SUCCESS,
+        resolc_result.code
+    );
+
+    assert!(
+        resolc_result.output.contains("Usage: resolc"),
+        "Expected the output to contain usage information."
+    );
+
+    let solc_result = utils::execute_solc(ARGUMENTS);
+    utils::assert_equal_exit_codes(&solc_result, &resolc_result);
+}
+
+#[test]
 fn fails_without_options() {
     let resolc_result = utils::execute_resolc(&[]);
     utils::assert_command_failure(&resolc_result, "Omitting options");

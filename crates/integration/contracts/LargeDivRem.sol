0c6048c272ace42d3cedb4d8e06a20ec8ce3d33a
kvp <mammal_windier8j@icloud.com>
2025-10-20 11:35:40 -0400

    Added compile-only test to test expansion of i256 int div/rem

---
+++ crates/integration/contracts/LargeDivRem.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8;
+
+contract LargeDivRem {
+    function rem_2(int n) public pure returns (int q) {
+        assembly {
+            q := smod(n, 2)
+        }
+    }
+
+    function div_2(int n) public pure returns (int q) {
+        assembly {
+            q := sdiv(n, 2)
+        }
+    }
+
+    function rem_7(int n) public pure returns (int q) {
+        assembly {
+            q := smod(n, 7)
+        }
+    }
+
+    function div_7(int n) public pure returns (int q) {
+        assembly {
+            q := sdiv(n, 7)
+        }
+    }
+}

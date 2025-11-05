2f2f95a5076f1449d8b5e9a58c87f42004faea7f
elle-j <linnea@parity.io>
2025-11-05 17:53:54 +0100

    Replace custom macro with explicit functions.

--- crates/resolc/benches/compile.rs
+++ crates/resolc/benches/compile.rs
@@ -1,7 +1,6 @@
 #![cfg(feature = "bench-resolc")]
 
 use std::{
-    path::Path,
     process::Command,
     time::{Duration, Instant},
 };
@@ -13,7 +12,7 @@ use criterion::{
 };
 use revive_integration::cases::UncompiledContract;
 
-fn measure_resolc(iters: u64, arguments: &[&str]) -> Duration {
+fn measure_resolc(arguments: &[&str], iters: u64) -> Duration {
     let start = Instant::now();
 
     for _i in 0..iters {
@@ -36,11 +35,18 @@ fn execute_command(command: &str, arguments: &[&str]) {
         .expect("command failed");
 }
 
+/// Get the relative path to a `.sol` contract file with file stem `name`.
+/// The file is expected to live under `"crates/integration/contracts/"`.
+fn get_contract_path(name: &str) -> String {
+    format!("crates/integration/contracts/{name}.sol")
+}
+
 fn bench(mut group: BenchmarkGroup<'_, WallTime>, compiler_arguments: &[&str]) {
+    group.measurement_time(Duration::from_secs(10));
     group.sample_size(10);
 
     group.bench_function("Resolc", |b| {
-        b.iter_custom(|iters| measure_resolc(iters, compiler_arguments));
+        b.iter_custom(|iters| measure_resolc(compiler_arguments, iters));
     });
 
     group.finish();
@@ -61,80 +67,71 @@ fn bench_overwrite_same_memory(c: &mut Criterion) {
     bench(group, compiler_arguments);
 }
 
-fn get_file_stem(path: &str) -> &str {
-    Path::new(path).file_stem().unwrap().to_str().unwrap()
+fn bench_baseline(c: &mut Criterion) {
+    let name = "Baseline";
+    let path = get_contract_path(name);
+    let group = group(c, name);
+    let compiler_arguments = &[&path, "-O0"];
+
+    bench(group, compiler_arguments);
 }
 
-/// Creates benchmark functions which are added as targets
-/// to the `criterion_group` macro.
-///
-/// ## Parameters
-/// 1. The generated function name => The filename
-///    (filename is expected to live under `"../../integration/contracts/"`)
-///
-/// ## Examples
-/// ```rust
-/// create_bench_functions!(
-///     bench_ext_code => "ExtCode.sol"
-///     bench_fibonacci => "Fibonacci.sol"
-/// );
-/// // Generates:
-/// fn bench_ext_code(c: &mut Criterion) {/*...*/}
-/// fn bench_fibonacci(c: &mut Criterion) {/*...*/}
-/// criterion_group!(
-///     /*...*/
-///     targets =
-///     bench_ext_code,
-///     bench_fibonacci
-/// );
-/// ```
-macro_rules! create_bench_functions {
-    ($($function_name:ident => $filename:expr),+) => {
-        $(
-            fn $function_name(c: &mut Criterion) {
-                let path = concat!("../../integration/contracts/", $filename);
-                let group = group(c, get_file_stem(path));
-                let compiler_arguments = &[path, "-O0"];
+fn bench_erc20(c: &mut Criterion) {
+    let name = "ERC20";
+    let path = get_contract_path(name);
+    let group = group(c, name);
+    let compiler_arguments = &[&path, "-O0"];
 
-                bench(group, compiler_arguments);
-            }
-        )*
-
-        criterion_group!(
-            name = compile;
-            config = Criterion::default();
-            targets = bench_overwrite_same_memory,
-            $(
-                $function_name
-            ),*
-        );
-    };
+    bench(group, compiler_arguments);
 }
 
-create_bench_functions!(
-    bench_add_mod_mul_mod => "AddModMulMod.sol",
-    bench_balance => "Balance.sol",
-    bench_baseline => "Baseline.sol",
-    bench_call => "Call.sol",
-    bench_computation => "Computation.sol",
-    bench_create => "Create.sol",
-    bench_create2 => "Create2.sol",
-    bench_delegate => "Delegate.sol",
-    bench_division_arithmetics => "DivisionArithmetics.sol",
-    bench_erc20 => "ERC20.sol",
-    bench_events => "Events.sol",
-    bench_ext_code => "ExtCode.sol",
-    bench_fibonacci => "Fibonacci.sol",
-    bench_function_pointer => "FunctionPointer.sol",
-    bench_function_type => "FunctionType.sol",
-    bench_gas_left => "GasLeft.sol",
-    bench_layout_at => "LayoutAt.sol",
-    bench_m_copy_overlap => "MCopyOverlap.sol",
-    bench_send => "Send.sol",
-    bench_sha1 => "SHA1.sol",
-    bench_storage => "Storage.sol",
-    bench_transfer => "Transfer.sol",
-    bench_value => "Value.sol",
+fn bench_sha1(c: &mut Criterion) {
+    let name = "SHA1";
+    let path = get_contract_path(name);
+    let group = group(c, name);
+    let compiler_arguments = &[&path, "-O0"];
+
+    bench(group, compiler_arguments);
+}
+
+fn bench_storage(c: &mut Criterion) {
+    let name = "Storage";
+    let path = get_contract_path(name);
+    let group = group(c, name);
+    let compiler_arguments = &[&path, "-O0"];
+
+    bench(group, compiler_arguments);
+}
+
+fn bench_transfer(c: &mut Criterion) {
+    let name = "Transfer";
+    let path = get_contract_path(name);
+    let group = group(c, name);
+    let compiler_arguments = &[&path, "-O0"];
+
+    bench(group, compiler_arguments);
+}
+
+fn bench_value(c: &mut Criterion) {
+    let name = "Value";
+    let path = get_contract_path(name);
+    let group = group(c, name);
+    let compiler_arguments = &[&path, "-O0"];
+
+    bench(group, compiler_arguments);
+}
+
+criterion_group!(
+    name = compile;
+    config = Criterion::default();
+    targets =
+        bench_overwrite_same_memory,
+        bench_baseline,
+        bench_erc20,
+        bench_sha1,
+        bench_storage,
+        bench_transfer,
+        bench_value,
 );
 
 criterion_main!(compile);

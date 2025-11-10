ccdb62622f9a1b22a81595ce933b80799849f66f
elle-j <linnea@parity.io>
2025-11-10 13:46:37 +0100

    Use resolc fixtures instead of revive-integration ones.

--- crates/resolc/benches/compile.rs
+++ crates/resolc/benches/compile.rs
@@ -1,6 +1,7 @@
 #![cfg(feature = "bench-resolc")]
 
 use std::{
+    path::PathBuf,
     process::{Command, Output},
     time::Duration,
 };
@@ -11,7 +12,7 @@ use criterion::{
     BenchmarkGroup, Criterion,
 };
 
-/// Function under test - Executes the resolc executable.
+/// The function under test executes the resolc executable.
 fn execute_resolc(arguments: &[&str]) {
     let result = execute_command("resolc", arguments);
     assert!(
@@ -33,10 +34,19 @@ fn get_stderr(result: &Output) -> String {
     String::from_utf8_lossy(&result.stderr).to_string()
 }
 
-/// Get the relative path to a `.sol` contract file with file stem `name`.
-/// The file is expected to live under `"crates/integration/contracts/"`.
-fn get_contract_path(name: &str) -> String {
-    format!("crates/integration/contracts/{name}.sol")
+/// Gets the absolute path of a file. The `relative_path` must
+/// be relative to this file.
+/// Panics if the path does not exist or is not an accessible file.
+fn get_absolute_path(relative_path: &str) -> String {
+    let this_file = PathBuf::from(file!());
+    let this_directory = this_file.parent().expect("expected a parent directory");
+    let absolute_path = this_directory.join(relative_path);
+
+    if !absolute_path.is_file() {
+        panic!("expected a file at `{}`", absolute_path.display());
+    }
+
+    absolute_path.to_string_lossy().into_owned()
 }
 
 fn group<'error, M>(c: &'error mut Criterion<M>, group_name: &str) -> BenchmarkGroup<'error, M>
@@ -50,53 +60,32 @@ fn bench(mut group: BenchmarkGroup<'_, WallTime>, compiler_arguments: &[&str]) {
     group.measurement_time(Duration::from_secs(10));
     group.sample_size(10);
 
-    group.bench_function("Resolc", |b| {
+    group.bench_function("resolc", |b| {
         b.iter(|| execute_resolc(compiler_arguments));
     });
 
     group.finish();
 }
 
-fn bench_baseline(c: &mut Criterion) {
-    let name = "Baseline";
-    let group = group(c, name);
-    let path = get_contract_path(name);
+fn bench_empty(c: &mut Criterion) {
+    let group = group(c, "Empty");
+    let path = get_absolute_path("../src/tests/data/solidity/contract.sol");
     let compiler_arguments = &[&path, "-O0"];
 
     bench(group, compiler_arguments);
 }
 
-fn bench_erc20(c: &mut Criterion) {
-    let name = "ERC20";
-    let group = group(c, name);
-    let path = get_contract_path(name);
+fn bench_dependency(c: &mut Criterion) {
+    let group = group(c, "Dependency");
+    let path = get_absolute_path("../src/tests/data/solidity/dependency.sol");
     let compiler_arguments = &[&path, "-O0"];
 
     bench(group, compiler_arguments);
 }
 
-fn bench_sha1(c: &mut Criterion) {
-    let name = "SHA1";
-    let group = group(c, name);
-    let path = get_contract_path(name);
-    let compiler_arguments = &[&path, "-O0"];
-
-    bench(group, compiler_arguments);
-}
-
-fn bench_storage(c: &mut Criterion) {
-    let name = "Storage";
-    let group = group(c, name);
-    let path = get_contract_path(name);
-    let compiler_arguments = &[&path, "-O0"];
-
-    bench(group, compiler_arguments);
-}
-
-fn bench_transfer(c: &mut Criterion) {
-    let name = "Transfer";
-    let group = group(c, name);
-    let path = get_contract_path(name);
+fn bench_large_div_rem(c: &mut Criterion) {
+    let group = group(c, "LargeDivRem");
+    let path = get_absolute_path("../src/tests/data/solidity/large_div_rem.sol");
     let compiler_arguments = &[&path, "-O0"];
 
     bench(group, compiler_arguments);
@@ -106,11 +95,9 @@ criterion_group!(
     name = benches;
     config = Criterion::default();
     targets =
-        bench_baseline,
-        bench_erc20,
-        bench_sha1,
-        bench_storage,
-        bench_transfer,
+        bench_empty,
+        bench_dependency,
+        bench_large_div_rem,
 );
 
 criterion_main!(benches);

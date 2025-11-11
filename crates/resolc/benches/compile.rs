ba488b895c72d9007fdc789b33d838d081ab2e8a
elle-j <linnea@parity.io>
2025-11-11 11:10:06 +0100

    Add '--standard-json' benchmarks.

--- crates/resolc/benches/compile.rs
+++ crates/resolc/benches/compile.rs
@@ -1,29 +1,36 @@
 use std::{
+    fs::File,
     path::PathBuf,
-    process::{Command, Output},
+    process::{Command, Output, Stdio},
     time::Duration,
 };
 
 use criterion::{
     criterion_group, criterion_main,
     measurement::{Measurement, WallTime},
-    BenchmarkGroup, Criterion,
+    BatchSize, BenchmarkGroup, Criterion,
 };
+use resolc::{self, SolcCompiler};
 
 /// The function under test executes the `resolc` executable.
-fn execute_resolc(arguments: &[&str]) {
-    execute_command("resolc", arguments);
+fn execute_resolc(arguments: &[&str], stdin_config: Stdio) {
+    execute_command(resolc::DEFAULT_EXECUTABLE_NAME, arguments, stdin_config);
 }
 
 /// The function under test executes the `solc` executable.
-fn execute_solc(arguments: &[&str]) {
-    execute_command("solc", arguments);
+fn execute_solc(arguments: &[&str], stdin_config: Stdio) {
+    execute_command(
+        SolcCompiler::DEFAULT_EXECUTABLE_NAME,
+        arguments,
+        stdin_config,
+    );
 }
 
 #[inline(always)]
-fn execute_command(command: &str, arguments: &[&str]) {
+fn execute_command(command: &str, arguments: &[&str], stdin_config: Stdio) {
     let result = Command::new(command)
         .args(arguments)
+        .stdin(stdin_config)
         .output()
         .expect("expected command output");
 
@@ -36,6 +43,13 @@ fn execute_command(command: &str, arguments: &[&str]) {
 
 fn get_stderr(result: &Output) -> String {
     String::from_utf8_lossy(&result.stderr).to_string()
+}
+
+fn get_stdin_config(stdin_file_path: Option<&str>) -> Stdio {
+    match stdin_file_path {
+        Some(path) => Stdio::from(File::open(path).unwrap()),
+        None => Stdio::null(),
+    }
 }
 
 /// Gets the absolute path of a file. The `relative_path` must
@@ -61,16 +75,25 @@ fn bench(
     mut group: BenchmarkGroup<'_, WallTime>,
     resolc_arguments: &[&str],
     solc_arguments: &[&str],
+    stdin_file_path: Option<&str>,
 ) {
     group.measurement_time(Duration::from_secs(10));
     group.sample_size(10);
 
     group.bench_function("resolc", |b| {
-        b.iter(|| execute_resolc(resolc_arguments));
+        b.iter_batched(
+            || get_stdin_config(stdin_file_path),
+            |stdin_config| execute_resolc(resolc_arguments, stdin_config),
+            BatchSize::SmallInput,
+        );
     });
 
     group.bench_function("solc", |b| {
-        b.iter(|| execute_solc(solc_arguments));
+        b.iter_batched(
+            || get_stdin_config(stdin_file_path),
+            |stdin_config| execute_solc(solc_arguments, stdin_config),
+            BatchSize::SmallInput,
+        );
     });
 
     group.finish();
@@ -82,7 +105,7 @@ fn bench_empty(c: &mut Criterion) {
     let resolc_arguments = &[&path, "-O3"];
     let solc_arguments = &[&path, "--optimize"];
 
-    bench(group, resolc_arguments, solc_arguments);
+    bench(group, resolc_arguments, solc_arguments, None);
 }
 
 fn bench_dependency(c: &mut Criterion) {
@@ -91,7 +114,7 @@ fn bench_dependency(c: &mut Criterion) {
     let resolc_arguments = &[&path, "-O3"];
     let solc_arguments = &[&path, "--optimize"];
 
-    bench(group, resolc_arguments, solc_arguments);
+    bench(group, resolc_arguments, solc_arguments, None);
 }
 
 fn bench_large_div_rem(c: &mut Criterion) {
@@ -100,7 +123,7 @@ fn bench_large_div_rem(c: &mut Criterion) {
     let resolc_arguments = &[&path, "-O3"];
     let solc_arguments = &[&path, "--optimize"];
 
-    bench(group, resolc_arguments, solc_arguments);
+    bench(group, resolc_arguments, solc_arguments, None);
 }
 
 fn bench_memset(c: &mut Criterion) {
@@ -109,7 +132,7 @@ fn bench_memset(c: &mut Criterion) {
     let resolc_arguments = &[&path, "--yul", "-O3"];
     let solc_arguments = &[&path, "--strict-assembly", "--optimize"];
 
-    bench(group, resolc_arguments, solc_arguments);
+    bench(group, resolc_arguments, solc_arguments, None);
 }
 
 fn bench_return(c: &mut Criterion) {
@@ -118,7 +141,16 @@ fn bench_return(c: &mut Criterion) {
     let resolc_arguments = &[&path, "--yul", "-O3"];
     let solc_arguments = &[&path, "--strict-assembly", "--optimize"];
 
-    bench(group, resolc_arguments, solc_arguments);
+    bench(group, resolc_arguments, solc_arguments, None);
+}
+
+fn bench_standard_json_contracts(c: &mut Criterion) {
+    let group = group(c, "Multiple Contracts (`--standard-json`)");
+    let path = absolute_path("src/tests/data/standard_json/solidity_contracts.json");
+    let resolc_arguments = &["--standard-json"];
+    let solc_arguments = &["--standard-json"];
+
+    bench(group, resolc_arguments, solc_arguments, Some(&path));
 }
 
 criterion_group!(
@@ -130,6 +162,7 @@ criterion_group!(
         bench_large_div_rem,
         bench_memset,
         bench_return,
+        bench_standard_json_contracts,
 );
 
 criterion_main!(benches);

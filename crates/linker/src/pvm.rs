3aa24c9226a3d582175e0d0d1ffaf8d8d3da8c04
xermicus <bigcyrill@hotmail.com>
2025-11-21 15:03:19 +0100

    update the Rust dependencies
    Signed-off-by: xermicus <bigcyrill@hotmail.com>

--- crates/linker/src/pvm.rs
+++ crates/linker/src/pvm.rs
@@ -5,6 +5,10 @@ pub fn polkavm_linker<T: AsRef<[u8]>>(code: T, strip_binary: bool) -> anyhow::Re
     config.set_strip(strip_binary);
     config.set_optimize(true);
 
-    polkavm_linker::program_from_elf(config, code.as_ref())
-        .map_err(|reason| anyhow::anyhow!("polkavm linker failed: {}", reason))
+    polkavm_linker::program_from_elf(
+        config,
+        polkavm_linker::TargetInstructionSet::ReviveV1,
+        code.as_ref(),
+    )
+    .map_err(|reason| anyhow::anyhow!("polkavm linker failed: {}", reason))
 }

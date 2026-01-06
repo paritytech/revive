48db4111a8d2816f008103630b24857f1b840c75
Cyrill Leutwiler <bigcyrill@hotmail.com>
2026-01-06 09:49:36 +0100

    the marketplace std json CLI test
    Signed-off-by: Cyrill Leutwiler <bigcyrill@hotmail.com>

--- crates/resolc/src/tests/cli/standard_json.rs
+++ crates/resolc/src/tests/cli/standard_json.rs
@@ -4,7 +4,8 @@ use revive_solc_json_interface::SolcStandardJsonOutput;
 
 use crate::cli_utils::{
     assert_command_success, assert_equal_exit_codes, execute_resolc_with_stdin_input,
-    execute_solc_with_stdin_input, STANDARD_JSON_CONTRACTS_PATH, STANDARD_JSON_NO_EVM_CODEGEN_PATH,
+    execute_solc_with_stdin_input, STANDARD_JSON_CONTRACTS_PATH,
+    STANDARD_JSON_NO_EVM_CODEGEN_COMPLEX_PATH, STANDARD_JSON_NO_EVM_CODEGEN_PATH,
 };
 
 const JSON_OPTION: &str = "--standard-json";
@@ -28,6 +29,25 @@ fn runs_with_valid_input_file() {
 fn no_evm_codegen_requested() {
     let result = execute_resolc_with_stdin_input(&[JSON_OPTION], STANDARD_JSON_NO_EVM_CODEGEN_PATH);
     assert_command_success(&result, "EVM codegen std json input fixture should build");
+
+    let output: SolcStandardJsonOutput = serde_json::from_str(&result.stdout).unwrap();
+    assert!(!output.errors.iter().any(|msg| msg.severity == "error"))
+}
+
+/// A variant with more complex contracts.
+///
+/// The fixture is from a real project known to trigger the "Stack too deep" error.
+///
+/// The test ensures we set the right flags when requesting the Yul IR from solc:
+/// no EVM codegen should be involved.
+#[test]
+fn no_evm_codegen_requested_complex() {
+    let result =
+        execute_resolc_with_stdin_input(&[JSON_OPTION], STANDARD_JSON_NO_EVM_CODEGEN_COMPLEX_PATH);
+    assert_command_success(
+        &result,
+        "the EVM codegen std json complext input fixture should build fine",
+    );
 
     let output: SolcStandardJsonOutput = serde_json::from_str(&result.stdout).unwrap();
     assert!(!output.errors.iter().any(|msg| msg.severity == "error"))

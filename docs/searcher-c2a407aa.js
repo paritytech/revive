5c430ffd8eaba260db01b2e8dc0b474fc37dc848
Marian Radu <marian@parity.io>
2026-01-07 13:46:48 +0200

    Add doc changes.

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-82723808.js',
+                path_to_root + 'searchindex-4c7bbe7b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');

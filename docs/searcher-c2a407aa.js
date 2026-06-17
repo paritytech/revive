4eabe2e05708f371941f178ec4ca1e8e9e06f6f1
elle-j <linnea@parity.io>
2026-06-17 15:01:49 +0200

    Make book.

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-a6a49a5a.js',
+                path_to_root + 'searchindex-5ce1788e.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
